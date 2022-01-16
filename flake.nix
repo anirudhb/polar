cf299d02a2207f5a09cb2e42c656cef4d3b113f7
anirudhb <anirudhb@users.noreply.github.com>
2022-01-15 20:07:25 -0800

    nix: init flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A very basic flake with a shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    fenix.url = "github:nix-community/fenix";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, fenix }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+      fenixPkgs = fenix.packages.${system};
+    in rec {
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          (fenixPkgs.latest.withComponents [
+            "cargo"
+            "rustc"
+            "rust-src"
+            "rustfmt"
+          ])
+          fenixPkgs.rust-analyzer
+          cargo-edit
+          libiconv
+        ];
+      };
+    });
+}
